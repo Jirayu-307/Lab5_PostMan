9dc66515c1584654ed8a06e0e81f7d495239cfea
Jirayu-307 <119925525+Jirayu-307@users.noreply.github.com>
2024-02-16 10:57:12 +0700

    body-parser

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const app = express()
 
 app.get('/', (req, res) => {
